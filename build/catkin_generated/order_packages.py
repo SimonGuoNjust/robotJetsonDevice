0cca248d42c9e5819acbb924e53157a16c969169
= <=>
2024-08-28 11:41:33 +0800

    untracked build and devel

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/nvidia/ros_rosbot_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/nvidia/ros_mmdeploy_ws/devel;/home/nvidia/realsense_ros_ws/devel;/home/nvidia/tf_0/devel;/home/nvidia/tf/devel;/opt/ros/melodic'.split(';') if '/home/nvidia/ros_mmdeploy_ws/devel;/home/nvidia/realsense_ros_ws/devel;/home/nvidia/tf_0/devel;/home/nvidia/tf/devel;/opt/ros/melodic' != '' else []
